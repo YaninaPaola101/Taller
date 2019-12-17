0f1f892321cec4880a01373722631f278514656c
YaninaPaola101 <luriannemarianne@gmail.com>
2019-12-17 17:06:39 -0400

    colores agregados y aplicados:

--- src/figuras/Figura.java
+++ src/figuras/Figura.java
@@ -6,7 +6,6 @@ public abstract class Figura {
 	
 	protected float[] color;
 	
-	public static float[] ROJO = {0.0f,0.0f,0.0f};
 	public abstract void dibujar(GL2 gl);
 	public abstract void moverArriba();
 	public abstract void moverDerecha();
@@ -16,6 +15,10 @@ public abstract class Figura {
 	public void asignarColor(String color){
 		switch(color){
 			case "rojo":this.color = Color.ROJO;break;
+			case "amarillo":this.color = Color.AMARILLO;break;
+			case "verde":this.color = Color.VERDE;break;
+			case "azul":this.color = Color.AZUL;break;
+			case "negro":this.color = Color.NEGRO;break;
 			default :System.out.println("color no reconocido");break;
 		}
 	}
--- src/figuras/Triangulo.java
+++ src/figuras/Triangulo.java
@@ -13,20 +13,19 @@ public class Triangulo extends Figura {
 		puntoa = new Punto(0.0f, 1.0f, 0.0f);
 		puntob = new Punto(-1.0f, -1.0f, 0.0f);
 		puntoc = new Punto(1.0f, -1.0f, 0.0f);
-		
+		asignarColor(color);
 	}
 	public void dibujar(GL2 gl){  
 		
 //	   gl.glTranslatef(6.5f, 0.0f, -6.0f); //
 	   gl.glBegin(GL_TRIANGLES);  
-	   gl.glColor3f(Color.ROJO[0],Color.ROJO[1],Color.ROJO[2]);
+	   gl.glColor3f(color[0],color[1],color[2]);
        gl.glVertex3f(puntoa.getCoordenadax(), puntoa.getCoordenaday(), puntoa.getCoordenadaz());
-       gl.glColor3f(0.0f, 255f, 0.0f);
        gl.glVertex3f(puntob.getCoordenadax(), puntob.getCoordenaday(), puntob.getCoordenadaz());
-       gl.glColor3f(1.0f, 0.0f, 255f);
        gl.glVertex3f(puntoc.getCoordenadax(), puntoc.getCoordenaday(), puntoc.getCoordenadaz());
        gl.glEnd();
 	}
+
 	@Override
 	public void moverArriba() {
 		
